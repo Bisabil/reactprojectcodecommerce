c656f04b7a5b57748ae26d634a3cf1c7113ed2a6
Bisabil Nizam] <nbisabil@gmail.com>
2023-03-18 23:35:05 +0800

    Finisih TopBar

---
+++ src/component/NavBar.jsx
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react'
+import {FaBars} from 'react-icons/fa'
+import { BsChatSquareDots } from 'react-icons/bs'
+import { FaFacebook, FaGooglePlus, FaInstagram, FaTwitter } from 'react-icons/fa' 
+
+const NavBar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NavBar
